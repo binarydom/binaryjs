b281ed8ee4d1614208f7f0a0d1506e343549477c
binarydom <binarydom@gmail.com>
2025-06-14 23:43:57 +0200

    feat(): added optimisation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,30 @@
-import { BinaryDOMRenderer, BinaryDOMNode } from 'binarydom';
+// Core exports
+export { BinaryJS } from "./core/BinaryJS";
+export { BinaryDOM } from "./core/BinaryDOM";
+export { BinaryJSState } from "./core/BinaryJSState";
+export { BinaryJSHooks } from "./core/BinaryJSHooks";
+export { BinaryJSApiClient } from "./core/BinaryJSApiClient";
+
+// Component exports
+export { BinaryJSComponent } from "./components/BinaryJSComponent";
+export { ErrorBoundary } from "./components/ErrorBoundary";
+export { Portal } from "./components/Portal";
+export { Suspense } from "./components/Suspense";
+
+// Utility exports
+export * from "./utils/BinaryJSUtils";
+
+// Type exports
+export type {
+  BinaryJSNode,
+  BinaryJSProps,
+  BinaryJSOptions,
+  NodeType,
+  StateType,
+  HookType,
+} from "./types/BinaryJSTypes";
+
+import { BinaryDOMRenderer, BinaryDOMNode } from "binarydom";
 
 export class BinaryJS {
   private renderer: BinaryDOMRenderer;
@@ -14,27 +40,29 @@ export class BinaryJS {
   // Example component to test the integration
   static createExampleComponent(): BinaryDOMNode {
     return {
-      type: 'element',
-      tagName: 'div',
-      id: 'example',
+      type: "element",
+      tagName: "div",
+      id: "example",
       props: {
-        className: 'example-component',
-        children: [{
-          type: 'text',
-          id: 'example-text',
-          value: 'Hello from BinaryJS!',
-          props: {},
-          attributes: new Map(),
-          children: [],
-          left: null,
-          right: null,
-          checksum: 0,
-          isDirty: false,
-          parent: null,
-          eventHandlers: new Map(),
-          state: null,
-          hooks: []
-        }]
+        className: "example-component",
+        children: [
+          {
+            type: "text",
+            id: "example-text",
+            value: "Hello from BinaryJS!",
+            props: {},
+            attributes: new Map(),
+            children: [],
+            left: null,
+            right: null,
+            checksum: 0,
+            isDirty: false,
+            parent: null,
+            eventHandlers: new Map(),
+            state: null,
+            hooks: [],
+          },
+        ],
       },
       attributes: new Map(),
       children: [],
@@ -45,7 +73,7 @@ export class BinaryJS {
       parent: null,
       eventHandlers: new Map(),
       state: null,
-      hooks: []
+      hooks: [],
     };
   }
-} 
+}
